c54f8f760b6326b42c66497f2d7fc7f43bf2dfe0
MinayeAliyeva <7s346pu@code.edu.az>
2024-08-17 21:01:17 +0400

    practise

--- test.js
+++ test.js
@@ -1,34 +1,34 @@
-// for ve forEach
-const arr = [1, 2, 3, 4, 55, 5, 4];
-let uniqueArr = [];
+// // for ve forEach
+// const arr = [1, 2, 3, 4, 55, 5, 4];
+// let uniqueArr = [];
 
-for (let i = 0; i <= arr.length - 1; i++) {
-  if (!uniqueArr.includes(arr[i])) {
-    uniqueArr.push(arr[i]);
-  }
-}
+// for (let i = 0; i <= arr.length - 1; i++) {
+//   if (!uniqueArr.includes(arr[i])) {
+//     uniqueArr.push(arr[i]);
+//   }
+// }
 
-console.log(uniqueArr);
+// console.log(uniqueArr);
 
-///W/ forEach
-const uniqueArr2 = [];
-arr?.forEach((el, i) => {
-  if (!uniqueArr2.includes(el)) {
-    uniqueArr2.push(el);
-  }
-});
-console.log(uniqueArr2);
+// ///W/ forEach
+// const uniqueArr2 = [];
+// arr?.forEach((el, i) => {
+//   if (!uniqueArr2.includes(el)) {
+//     uniqueArr2.push(el);
+//   }
+// });
+// console.log(uniqueArr2);
 
-//s2
-const uniqueArr3 = [];
-arr?.forEach((el, i, arrList) => {
-  if (!uniqueArr3.includes(arr[i])) uniqueArr3.push(arr[i]);
-});
-console.log(uniqueArr3);
+// //s2
+// const uniqueArr3 = [];
+// arr?.forEach((el, i, arrList) => {
+//   if (!uniqueArr3.includes(arr[i])) uniqueArr3.push(arr[i]);
+// });
+// console.log(uniqueArr3);
 
-////////
-const uniqueArr4 = [];
-arr?.forEach((el, i, arrList) => {
-  if (!uniqueArr4.includes(arrList[i])) uniqueArr4.push(arrList[i]);
-});
-console.log(uniqueArr4);
+// ////////
+// const uniqueArr4 = [];
+// arr?.forEach((el, i, arrList) => {
+//   if (!uniqueArr4.includes(arrList[i])) uniqueArr4.push(arrList[i]);
+// });
+// console.log(uniqueArr4);
--- test2.js
+++ test2.js
@@ -18,22 +18,20 @@ const cars = [
   { name: "x6", marka: "bmw", id: 2 },
 ];
 
-const groupByID = [];
+const groupById = [];
 cars.forEach((car) => {
-  let group = groupByID.find((item) => {
+  let group = groupById.find((item) => {
     console.log("item", item);
-    console.log("Object.keys(item)", Object.keys(item));
-    return Object.keys(item)[0] == car.id;
+    return Object.keys(item) == car.id;
   });
-  console.log();
-  
   if (!group) {
     group = { [car.id]: [] };
-    groupByID.push(group);
+    groupById.push(group);
   }
   group[car.id].push(car);
 });
-console.log(groupByID);
+console.log("groupById", groupById);
+
 /*
 step1
 const groupByID = [];
@@ -58,5 +56,77 @@ const group=//
   group[car.id].push(car); bu kod calisir
 */
 
+const fruits = ["Apple", "Banana", "apple", "Orange", "BANANA"];
+const uniqueFruits = [];
+
+fruits.forEach((fruit) => {
+  const findFruit = uniqueFruits.find((uniqFruit) =>
+    uniqFruit.toLocaleLowerCase().includes(fruit.toLocaleLowerCase())
+  );
+  if (!findFruit) {
+    uniqueFruits.push(fruit);
+  }
+});
+console.log("uniqueFruits", uniqueFruits);
+////////
+const ides = [{ id: 1 }, { id: 2 }, { id: 1 }, { id: 3 }];
+const uniqueIdes = [];
+ides.forEach((idObj) => {
+  const find = uniqueIdes.find((obj) => obj.id == idObj.id);
+  if (!find) {
+    uniqueIdes.push(idObj);
+  }
+});
+console.log("uniqueIdes", uniqueIdes);
+
+/*[[1, 2, 2], [3, 4, [4, 5]], 1] gibi iç içe geçmiş dizilerden oluşan bir dizide,
+ tekrarlanan alt dizileri veya elemanları nasıl kaldırabilirsiniz*/
+
+const nestedArr = [[1, 2, 2], [3, 4, [4, 5]], 1];
+
+/*
+
+[{ id: 1, name: 'A' }, { id: 2, name: 'B' }, { id: 1, name: 'A' }] gibi id ve name özelliklerine sahip nesnelerden oluşan bir dizide,
+  hem id hem de name özelliklerine göre tekrarlanan nesneleri nasıl kaldırabilirsiniz?
+
+*/
+const array = [
+  { id: 1, name: "A" },
+  { id: 2, name: "B" },
+  { id: 1, name: "A" },
+];
+const uniqeArray = [];
+
+array.forEach((obj) => {
+  const check = uniqeArray.find(
+    (findObj) => findObj.id == obj.id || findObj.name == obj.id
+  );
+  if (!check) {
+    uniqeArray.push(obj);
+  }
+});
+console.log("uniqeArray", uniqeArray);
+
+/*
+[1, { id: 1 }, 2, { id: 1 }, 1] gibi hem sayılar hem de nesneler içeren bir diziyi nasıl benzersizleştirirsiniz
+*/
+const fiexArr = [1, { id: 1 }, 2, { id: 1 }, 1];
+const returnUniqueArr = [];
+fiexArr.forEach((el) => {
+  if (typeof el == "object") {
+    const find = returnUniqueArr.find((obj) => obj.id == el.id);
+    if (!find) returnUniqueArr.push(el);
+  } else {
+    const check = returnUniqueArr.includes(el);
+    if (!check) {
+      returnUniqueArr.push(el);
+    }
+  }
+});
+console.log("returnUniqueArr", returnUniqueArr);
 
 
+/*
+[1, [2, 3], [2, 3], { a: 1, b: 2 }, { a: 1, b: 2 }] gibi hem alt diziler hem de 
+nesneler içeren bir dizide tekrarlanan elemanları nasıl kaldırabilirsiniz?
+*/
